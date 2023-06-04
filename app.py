68da9cc2ffddba484e33f0f52c5b9bb2c67cf715
amuhairw <ammuhairwe@gmail.com>
2023-06-04 21:09:58 +0200

    Add requirements.txt and Procfile

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ lat, lon = -1.9441, 30.069
 offset = 0.11
 
 corners = [
-    [lon+offset, lat+offset], # bottom up right
-    [lon+offset, lat-offset], # top right
-    [lon-offset, lat-offset], # top left
-    [lon-offset, lat+offset]] # bottom left
+    [lon+offset, lat+offset],  # bottom up right
+    [lon+offset, lat-offset],  # top right
+    [lon-offset, lat-offset],  # top left
+    [lon-offset, lat+offset]   # bottom left
+]
 region = ee.Geometry.Polygon(corners)
 
 center = region.centroid()
@@ -31,24 +32,26 @@ print(type(countryData))
 
 countryData
 
-state_nine=countryData.filter(ee.Filter.eq("STATEFP","09"))
+state_nine = countryData.filter(ee.Filter.eq("STATEFP", "09"))
 
 import numpy as np
-a=np.array(state_nine.getInfo())
+a = np.array(state_nine.getInfo())
 
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots(1, figsize=(5,5))
+fig, ax = plt.subplots(1, figsize=(5, 5))
 sns.heatmap(trainCM/trainCM.sum(axis=1), annot=True)
 ax.set_xlabel('model predictions', fontsize=20)
 ax.set_ylabel('actual', fontsize=20)
 plt.title("Training data confusion matrix", fontsize=20)
 
-#80, 90, 10
+# 80, 90, 10
 # TODO: complete the following codes
-train_exercise =data.filter(ee.Filter.lt('random', 0.8))
-val_exercise = data.filter(ee.Filter.And(ee.Filter.gte('random', 0.8),ee.Filter.lt('random', 0.9)))
+train_exercise = data.filter(ee.Filter.lt('random', 0.8))
+val_exercise = data.filter(ee.Filter.And(ee.Filter.gte('random', 0.8), ee.Filter.lt('random', 0.9)))
 test_exercise = data.filter(ee.Filter.gte('random', 0.9))
+
+import torch.nn as nn
 
 class SimpleNeuralNetwork(nn.Module):
     def __init__(self):
@@ -65,22 +68,25 @@ class SimpleNeuralNetwork(nn.Module):
         self.fc3 = nn.Linear(100, 2)
         # A sigmoid activation layer
         # self.sigmoid =
-        def forward(self, x):
-            x = self.fc1(x)
-            x = self.relu1(x)
-            x = self.fc2(x)
-            x = self.relu2(x)
-            x = self.fc3(x)
-            # x = self.sigmoid(x)
-            return x
 
-import torch.nn as nn
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        # x = self.sigmoid(x)
+        return x
+
+import torch
 torch.manual_seed(42)
 # Answer
-tensor_input = torch.zeros((9,32,32)).unsqueeze(0).float().to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = SimpleNeuralNetwork().to(device)
+tensor_input = torch.zeros((9, 32, 32)).unsqueeze(0).float().to(device)
 out = model(tensor_input)
 
-print("Output is: ", out)
+print("Output is:", out)
 
 import ee
 
@@ -110,9 +116,10 @@ region = [
     [lon + offset, lat - offset],
     [lon + offset, lat + offset],
     [lon - offset, lat + offset],
-    [lon - offset, lat - offset]]
+    [lon - offset, lat - offset]
+]
 
-roi = ee.Geometry.Polygon([region])
+roi = ee.Geometry.Polygon(region)
 
 se2bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A']
 trainingbands = se2bands + ['avg_rad']
@@ -160,7 +167,7 @@ def get_fused_data():
     viirs = ee.Image(ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG").filterDate(
         "2015-07-01", "2015-12-31").filterBounds(roi).median().select('avg_rad').clip(roi))
 
-    # Substract the mean and divide by the standard deviation for the viirs samples
+    # Subtract the mean and divide by the standard deviation for the viirs samples
     viirsclean = viirs.subtract(vmu).divide(vstd)
 
     # Fuse the two datasets
@@ -168,18 +175,15 @@ def get_fused_data():
 
     return fusedclean
 
-    # Prepare the fused data
-    gee_data = get_fused_data()
+# Prepare the fused data
+gee_data = get_fused_data()
 
 def get_features(longitude, latitude):
     # Create an ee.Geometry instance from the coordinates
     poi_geometry = ee.Geometry.Point([longitude, latitude])
 
     # Sample features for the given point of interest, keeping only the training bands
-    dataclean = gee_data.select(trainingbands).sampleRegions(collection= poi_geometry, properties=[label], scale=scaleFactor)
-
-
-    # Use getInfo to load the sample's features
+    dataclean = gee_data.select(trainingbands).sampleRegions(collection= poi_geometry, properties=[label], scale=scaleFactor)  # Use getInfo to load the sample's features
     sample = dataclean.getInfo()
 
     # Find the band ordering in the loaded data
@@ -230,4 +234,4 @@ else:
     return render_template('index.html', prediction_text='The area at {}, {} location is is out of bounds.'.format(longitude, latitude))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True
